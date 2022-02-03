c454d5703fd2b824656f3b69580357d634633a6d
Clément Verrier <verrierclementrog@gmail.com>
2022-02-03 06:04:16 +0100

    Apply LDA on training dataset (all signals)

---
+++ scripts/epochs/discriminant_analysis.py
@@ -0,0 +1,41 @@
+import pathlib
+import numpy as np
+import sklearn.discriminant_analysis
+
+from bci3wads.utils import data
+from bci3wads.utils import constants
+from bci3wads.features.epoch import Epoch
+from bci3wads.features.estimator import Estimator
+from bci3wads.features.formatter import Formatter
+
+
+subject_name = 'Subject_A_Train'
+channels_tag = 'channels_11'
+model_type = 'lda'
+
+subject_path = pathlib.Path.cwd() / 'data' / 'processed' / \
+    subject_name / channels_tag
+
+for epoch_path in sorted(list(subject_path.glob('epoch_*.pickle')),
+                         key=lambda p: int(p.stem.split('_')[-1])):
+    epoch = Epoch(data.load_pickle(epoch_path))
+
+    signals = epoch.signals
+    X_train = signals.reshape(-1, signals.shape[-1])
+
+    y_train = np.zeros(signals.shape[:2], dtype=bool)
+    y_train[epoch.target_char_codes] = True
+    y_train = y_train.ravel()
+
+    estimator = Estimator(subject_path)
+    p0 = estimator.get_mismatch_proba()
+    priors = [p0, 1 - p0]
+
+    model = sklearn.discriminant_analysis.LinearDiscriminantAnalysis(
+        priors=priors
+    )
+
+    model.fit(X_train, y_train)
+
+    y_train_preds = model.predict(X_train)
+    print('Training accuracy:', np.sum(y_train == y_train_preds) / len(y_train))
